20c0da98bde81f008cb134d8410542ab49197d5a
SwarnimDoegar <50024003+SwarnimDoegar@users.noreply.github.com>
2019-09-13 20:15:46 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+/*This is a comment
+and this is its extension in next line*/
+int main()
+{
+    cout<< "Testing: " << 16/2 << " = " << 4*2 <<".\n\n";
+    char c1='/';
+    ifstream file;
+    ofstream ofile;
+    long fptr=file.tellg();
+    ofile.open("WithoutComment.cpp",ios::out);
+    file.open("main.cpp",ios::in);
+    char c;
+    while(!file.eof())
+    {
+        file.read((char*)&c,sizeof(char));
+        fptr=file.tellg();
+        if(c=='/')
+        {
+            file.read((char*)&c,sizeof(char));
+            if(c=='*')
+            {
+                while(file.read((char*)&c,sizeof(char)))
+                {
+                    if(c=='/')
+                        break;
+                }
+            }
+            else
+            {
+                cout<<"/";/*Prints the character to screen*/
+                ofile.write((char*)&c1,sizeof(char));
+                cout<<c;
+                ofile.write((char*)&c,sizeof(char));
+            }
+        }
+        else
+        {
+            cout<<c;
+            ofile.write((char*)&c,sizeof(char));
+        }
+    }
+    return 0;
+}
